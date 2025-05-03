aaabdf64f04170962f5f59f947e2febe7a611565
Anastasiia <skubakovaa@gmail.com>
2025-05-03 22:36:25 +0200

    add models DishType, Cook, Dish

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "dish types"
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ["username"]
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    dish_type = models.ForeignKey(
+        DishType,
+        on_delete=models.CASCADE,
+        related_name="dishes"
+    )
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} (price: {self.price})"
