340a4547d66167d943574b9adad3a22f9689aba5
Anastasiia <skubakovaa@gmail.com>
2025-05-04 21:42:53 +0200

    implement custom forms for Dish creation and update

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,6 +1,8 @@
+from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-from catalog.models import Cook
+from catalog.models import Cook, Dish
 
 
 class CookCreationForm(UserCreationForm):
@@ -18,3 +20,14 @@ class CookUpdateForm(UserChangeForm):
             "last_name",
             "years_of_experience",
         )
+
+
+class DishForm(forms.ModelForm):
+    cooks = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+    class Meta:
+        model = Dish
+        fields = "__all__"
